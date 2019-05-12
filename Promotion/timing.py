f502e8a2c17110a04d36cea6e491469a7a2a7205
whchnb <40844169+whchnb@users.noreply.github.com>
2019-05-12 22:36:09 +0800

    Update timing.py

--- Promotion/timing.py
+++ Promotion/timing.py
@@ -7,11 +7,15 @@
 @desc: 每日01:00 定时执行营销任务
 """
 import os
-import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
 
+def job():
+    os.system(r'C:\Users\Administrator\AppData\Local\Programs\Python\Python36-32\python.exe D:/Main/Promotion/alibabaMarket.py')
 
-timing_time = '01:00:'
-while 1:
-    now = str(datetime.datetime.now())
-    if timing_time in now.split(' ')[1]:
-        os.system(r'C:\Users\Administrator\AppData\Local\Programs\Python\Python36-32\python.exe D:/Main/Promotion/alibabaMarket.py')
+if __name__ == '__main__':
+    # BlockingScheduler
+    scheduler = BlockingScheduler()
+    # scheduler.add_job(job, 'cron', hour=1, minute=00)
+    scheduler.add_job(job, 'cron', hour=1, minute=1)
+    scheduler.add_job(job, 'cron', hour=3, minute=1)
+    scheduler.start()
